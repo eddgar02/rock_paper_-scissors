c77516d659ea44272e03c1f13f833176ab550705
edgarlarasanchez <edgar.lara7681@gmail.com>
2024-09-02 13:51:12 -0500

    Added features to make it an interactive game that user can play outside of the console.

--- work.js
+++ work.js
@@ -9,46 +9,52 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("Do you choose rock, paper or scissors?");
-    return choice;
-}
+let humanScore = 0;
+let computerScore = 0;
+let liveScore = document.querySelector(".result")
+function playRound(human) {
+    let computer = getComputerChoice();
 
+    if (human === computer) {
+        //No change
+        liveScore.textContent = "It's a tie!";
 
+    } else if (
+        (computer === 'rock' && human === 'scissors') ||
+        (computer === 'paper' && human === 'rock') ||
+        (computer === 'scissors' && human === 'paper')
+    ) {
+        computerScore++;
+        liveScore.textContent = "Computer won this round!";
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound() {
-        let computer = getComputerChoice();
-        let human = getHumanChoice().toLowerCase;
-
-        if (human === computer) {
-            return "Draw";
-        } else if (
-            (computer === 'rock' && human === 'scissors') ||
-            (computer === 'paper' && human === 'rock') ||
-            (computer === 'scissors' && human === 'paper')
-        ) {
-            computerScore++;
-            return "You Lost Buddy.";
-        } else {
-            humanScore++;
-            return "You won!";
-        }
-    }
-
-    for(let i =0;i< 5;i++){
-        let message = playRound();
-        console.log(message);
-    }
-
-    if(computerScore > humanScore){
-        return "You lost to a computer! Don't worry."
     } else {
-        return "Congrats you beat a computer";
+        humanScore++;
+        liveScore.textContent = "You won this round!";
+    }
+    liveScore.textContent += `     You: ${humanScore} v.s Computer: ${computerScore}`;
+
+    // Check for game over condition
+    if (humanScore === 5) {
+        liveScore.textContent = `YOU WON: GAME OVER 
+        FINAL SCORE: You: ${humanScore} v.s Computer: ${computerScore}`;
+        humanScore = 0;
+        computerScore = 0;
+    } else if (computerScore === 5) {
+        liveScore.textContent = `COMPUTER WON: GAME OVER 
+        FINAL SCORE: You: ${humanScore} v.s Computer: ${computerScore}`;
+        computerScore = 0;
+        humanScore = 0;
     }
 }
 
-console.log(playGame());
+//NO LONGER PLAYING FIVE ROUNDS NOW PLAYING UNTIL SOMEONE GETS 5PTS
+// for(let i =0;i< 5;i++){
+//     let message = playRound();
+//     console.log(message);
+// }
+
+document.querySelector("#rock").addEventListener("click", () => playRound("rock"));
+document.querySelector("#paper").addEventListener("click", ()  =>playRound("paper"));
+document.querySelector("#scissors").addEventListener("click",() => playRound("scissors"));
+
+
